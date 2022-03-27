a13ad16fcbca2ddd7372bc4a1858524a24daf2af
Scott <scottpmccann@gmail.com>
2022-03-26 21:12:13 -0700

    broke sql connection

--- TodoFries.py
+++ TodoFries.py
@@ -2,7 +2,8 @@ import click
 from datetime import datetime
 import sqlite3
 
-conn = sqlite3.connect('tasks.db')
+conn = sqlite3.connect(r"C:\Users\scott\OneDrive\PyCharmProjects\Todo\todo.db")
+
 
 # conn.execute('''
 #     CREATE TABLE TODO
@@ -29,53 +30,52 @@ def cli():
 
 
 @cli.command()
-@click.option('--task', prompt='Create task', help='Task user is adding')
+@click.option('--title', prompt='Create task', help='Task user is adding')
 @click.option('--desc', prompt='Desc', help='Task Description')
 @click.option('--priority', prompt='Priority', help='Priority of task')
-def new_task(task, desc, priority):
+def new_task(title, desc, priority):
     """Adds a new task"""
     click.echo("Task saved. Use show-tasks to view")
     date = datetime.now().strftime("%c")
-    t = (None, date, task, desc, priority)
-    conn.execute("INSERT INTO TODO VALUES (?, ?, ?, ?, ?)", t)
+    t = (None, None, title, desc, priority, date, None)
+    conn.execute("INSERT INTO todo_app VALUES (?, ?, ?, ?, ?, ?, ?)", t)
     conn.commit()
     conn.close()
-
 
 
 @cli.command()
 def show_tasks():
     """Shows your tasks"""
     click.echo("All OF YOUR OPEN TASKS:")
-    cursor = conn.execute("SELECT id, task, description, priority, date from TODO")
+    cursor = conn.execute("SELECT id, title, description, priority, date from todo_app")
     for row in cursor:
         click.echo(f"ID: {row[0]}\nTask: {row[1]}\nDescription: {row[2]}\nPriority: {row[3]}\nDate: {row[4]}")
     conn.close()
 
 
 @cli.command()
-@click.option('--task_id', prompt='ID', help='Task to remove')
+@click.option('--id', prompt='ID', help='Task to remove')
 @click.option('--edit_t', prompt='Task(edit)', help='Task to edit')
 @click.option('--edit_d', prompt='Desc(edit)', help='Desc to edit')
 @click.option('--edit_p', prompt='Priority(edit)', help='Priority to edit')
-def edit_task(task_id, edit_t, edit_d, edit_p):
+def edit_task(id, edit_t, edit_d, edit_p):
     """Edits a task."""
-    t = (edit_t, edit_d, edit_p, task_id)
-    conn.execute("UPDATE TODO set TASK = ?, DESCRIPTION = ?, PRIORITY = ? WHERE ID = ?", t)
+    t = (edit_t, edit_d, edit_p, id)
+    conn.execute("UPDATE todo_app set TITLE = ?, DESCRIPTION = ?, PRIORITY = ? WHERE ID = ?", t)
     conn.commit()
     conn.close()
-    click.echo(f"Task {task_id} has been updated.")
+    click.echo(f"Task {id} has been updated.")
 
 
 
 
 @cli.command()
-@click.option('--task_id', prompt='ID', help='Task to remove')
-def resolve_task(task_id):
+@click.option('--id', prompt='ID', help='Task to remove')
+def resolve_task(id):
     """Resolves a task by id"""
-    click.echo(f"Task {task_id} has been resolved.")
-    t = task_id
-    conn.execute("DELETE from TODO where ID = ?", t)
+    click.echo(f"Task {id} has been resolved.")
+    t = id
+    conn.execute("DELETE from todo_app where ID = ?", t)
     conn.commit()
     conn.close()
 
